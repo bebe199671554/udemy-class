e3620c3f825a8457349c7f7659aec7eb55b71d8f
kuma <bebe1996715548@gmail.com>
2021-03-03 19:17:50 +0800

    make Material share

--- apps/udemy-class/src/app/app.module.ts
+++ apps/udemy-class/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedMaterialModule } from '@udemy-class/ui';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule,SharedMaterialModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ libs/ui/README.md
@@ -0,0 +1,4 @@
+# ui
+
+This library was generated with [Nx](https://nx.dev).
+
